ca987ed694d08e2bb3703bd350fab29e0f5fb21a
syth0le <chdan565@gmail.com>
2022-10-26 17:50:48 +0300

    added script for git clone of all repos

---
+++ scripts/git_clone.sh
@@ -0,0 +1,5 @@
+cd ../..
+git clone https://github.com/veiia/main-BE.git
+git clone https://github.com/veiia/authorization-BE.git
+git clone https://github.com/veiia/deployment-service-BE.git
+git clone https://github.com/veiia/veiia-FE.git
